dcb4bada87fbcaa82954de029eb906bfa2d11110
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-04-15 13:40:12 +0200

    new Command system for waiting for commands to execute

--- pyobs_baader/dome.py
+++ pyobs_baader/dome.py
@@ -1,5 +1,6 @@
 import math
 import threading
+from queue import Queue
 import serial
 import logging
 
@@ -11,6 +12,63 @@ from pyobs.mixins import FollowMixin
 from pyobs.modules.roof import BaseDome
 
 log = logging.getLogger(__name__)
+
+
+class Command:
+    def __init__(self, command: str):
+        self.command = command
+        self.response = None
+        self.finished = threading.Event()
+
+    def set(self, response: str):
+        """Set response and trigger finished.
+
+        Args:
+            response: Reponse from command
+        """
+        self.response = response
+        self.finished.set()
+
+    def wait(self, timeout: int = 10):
+        """Wait for command to finish.
+
+        Args:
+            timeout: Timeout in seconds.
+
+        Returns:
+            Response of command.
+
+        Raises:
+            TimeoutError if wait timed out.
+        """
+        self.finished.wait(timeout)
+        if not self.finished.is_set():
+            raise TimeoutError
+
+
+class OpenCommand(Command):
+    def __init__(self):
+        Command.__init__(self, command='d#opeshut')
+
+
+class CloseCommand(Command):
+    def __init__(self):
+        Command.__init__(self, command='d#closhut')
+
+
+class SlewCommand(Command):
+    def __init__(self, azimuth: float):
+        Command.__init__(self, command='d#azi%04d' % int(math.floor(azimuth * 10)))
+
+
+class GetShutCommand(Command):
+    def __init__(self):
+        Command.__init__(self, command='d#getshut')
+
+
+class GetAzimCommand(Command):
+    def __init__(self):
+        Command.__init__(self, command='d#getazim')
 
 
 class BaaderDome(FollowMixin, BaseDome):
@@ -42,8 +100,7 @@ class BaaderDome(FollowMixin, BaseDome):
         self._tolerance = tolerance
 
         # next command
-        self._command_lock = threading.RLock()
-        self._next_command = None
+        self._next_commands = Queue()
 
         # move locks
         self._lock_shutter = threading.RLock()
@@ -58,6 +115,7 @@ class BaaderDome(FollowMixin, BaseDome):
 
         # start thread
         self._add_thread_func(self._communication)
+        self._add_thread_func(self._update_status)
 
         # mixins
         FollowMixin.__init__(self, device=follow, interval=10, tolerance=tolerance, mode=IAltAz)
@@ -76,10 +134,11 @@ class BaaderDome(FollowMixin, BaseDome):
             log.info('Opening roof...')
             self._change_motion_status(IMotion.Status.INITIALIZING)
     
-            # set next command
-            with self._command_lock:
-                self._next_command = 'd#opeshut'
-    
+            # execute command
+            command = self._queue_command(OpenCommand())
+            if command.wait() != 'd#gotmess':
+                raise ValueError('Got invalid response from dome.')
+
             # wait for it
             while self._shutter != 'd#shutope':
                 # abort?
@@ -107,9 +166,10 @@ class BaaderDome(FollowMixin, BaseDome):
             log.info('Closing roof...')
             self._change_motion_status(IMotion.Status.PARKING)
 
-            # set next command
-            with self._command_lock:
-                self._next_command = 'd#closhut'
+            # execute command
+            command = self._queue_command(CloseCommand())
+            if command.wait() != 'd#gotmess':
+                raise ValueError('Got invalid response from dome.')
 
             # wait for it
             while self._shutter != 'd#shutclo':
@@ -153,10 +213,10 @@ class BaaderDome(FollowMixin, BaseDome):
             # Baader measures azimuth as West of South, so we need to convert it
             azimuth = BaaderDome._adjust_azimuth(az)
 
-            # set next command
-            with self._command_lock:
-                # format command, which is of format 'd#azi0900' with the number in units of 1/10 degree
-                self._next_command = 'd#azi%04d' % int(math.floor(azimuth * 10))
+            # execute command
+            command = self._queue_command(SlewCommand(azimuth))
+            if command.wait() != 'd#gotmess':
+                raise ValueError('Got invalid response from dome.')
 
             # wait for it
             while 180 - abs(abs(az - self._azimuth) - 180) > self._tolerance:
@@ -199,49 +259,66 @@ class BaaderDome(FollowMixin, BaseDome):
         return self.get_motion_status() not in [IMotion.Status.PARKED, IMotion.Status.INITIALIZING,
                                                 IMotion.Status.PARKING, IMotion.Status.ERROR, IMotion.Status.UNKNOWN]
 
+    def _queue_command(self, command: Command) -> Command:
+        """Queues a command and returns it.
+
+        Args:
+            command: Command to queue.
+
+        Returns:
+            The command.
+        """
+        self._next_commands.put(command)
+        size = self._next_commands.qsize()
+        if size > 10:
+            log.warning('Command queue is getting long with %d items!', size)
+        return command
+
     def _communication(self):
         """Thread method for communicating with the dome."""
 
         # run forever
         while not self.closing.is_set():
-            # get next command
-            with self._command_lock:
-                command = self._next_command
-                self._next_command = None
+            # no new command?
+            if self._next_commands.empty():
+                self.closing.wait(1)
+                continue
 
-            # got a command?
-            if command is not None:
-                # execute it!
-                self._send_command(command)
+            # get command
+            cmd: Command = self._next_commands.get()
 
-            else:
-                # nothing to do, so update status
-                self._update_status()
-
-            # sleep a little
-            self.closing.wait(1)
+            # execute it
+            response = self._send_command(cmd.command)
+            cmd.set(response)
 
     def _update_status(self):
         """Update status from dome."""
 
-        # get shutter status, might be one of d#shutclo, d#shutrun, or d#shutope
-        self._shutter = self._send_command('d#getshut')
-        if self._shutter == 'd#shutclo':
-            self._change_motion_status(IMotion.Status.PARKED)
+        # loop forever
+        while not self.closing.is_set():
+            # get shutter status, might be one of d#shutclo, d#shutrun, or d#shutope
+            command = self._queue_command(GetShutCommand())
+            self._shutter = command.wait()
+            if self._shutter == 'd#shutclo':
+                self._change_motion_status(IMotion.Status.PARKED)
 
-        # get azimuth
-        az_response = self._send_command('d#getazim')
+            # get azimuth
+            command = self._queue_command(GetAzimCommand())
+            az_response = command.wait()
 
-        # parse azimuth
-        if not az_response.startswith('d#azi'):
-            # wrong response!
-            self._azimuth = None
-        else:
-            # response is of form d#azi1800, so take lats four characters, parse to float and divide by 10
-            az = float(az_response[5:]) / 10.
+            # parse azimuth
+            if not az_response.startswith('d#azi'):
+                # wrong response!
+                self._azimuth = None
+            else:
+                # response is of form d#azi1800, so take lats four characters, parse to float and divide by 10
+                az = float(az_response[5:]) / 10.
 
-            # Baader measures azimuth as West of South, so we need to convert it
-            self._azimuth = BaaderDome._adjust_azimuth(az)
+                # Baader measures azimuth as West of South, so we need to convert it
+                self._azimuth = BaaderDome._adjust_azimuth(az)
+
+            # sleep a little
+            self.closing.wait(2)
 
     @staticmethod
     def _adjust_azimuth(az: float) -> float:
